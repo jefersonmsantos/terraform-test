926d1b9af05daa23c196dd31ea7ff7c71b6dbe0a
Jeferson <jefersonmsantos@outlook.com>
2022-02-11 10:48:36 -0300

    first-commit

--- infrastructure/bucket_files.tf
+++ infrastructure/bucket_files.tf
@@ -1,5 +1,6 @@
-resource "google_storage_bucket_object" "dag-beta-mkt" {
-  name   = "files/hello-world.py"
-  source = "../files/hello-world.py"
-  bucket = "terraform-test-files"
+resource "google_storage_bucket_object" "hello-world-file" {
+  name       = "files/hello-world.py"
+  source     = "../files/hello-world.py"
+  bucket     = "terraform-test-files"
+  depends_on = [google_storage_bucket.project-files]
 }
