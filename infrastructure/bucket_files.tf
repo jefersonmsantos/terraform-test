b420cf3a06f99631e42e08a6cf1dc87ad74ca612
Jeferson <jefersonmsantos@outlook.com>
2022-02-11 10:15:35 -0300

    first-commit

--- infrastructure/bucket_files.tf
+++ infrastructure/bucket_files.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket_object" "dag-beta-mkt" {
-  name   = "dags/mkt_beta.py"
+  name   = "files/hello-world.py"
   source = "../files/hello-world.py"
   bucket = var.google_bucket
 }
