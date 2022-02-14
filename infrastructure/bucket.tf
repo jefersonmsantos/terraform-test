af677730dc7b5431970a9b3259cda7b2ccbc2f56
Jeferson <jefersonmsantos@outlook.com>
2022-02-14 08:54:23 -0300

    test clean objects

--- infrastructure/bucket.tf
+++ infrastructure/bucket.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "project-files" {
-  name     = "terraform-test-files"
-  location = "us-central1"
-}
+# resource "google_storage_bucket" "project-files" {
+#   name     = "terraform-test-files"
+#   location = "us-central1"
+# }
--- infrastructure/bucket_files.tf
+++ infrastructure/bucket_files.tf
@@ -1,6 +1,6 @@
-resource "google_storage_bucket_object" "hello-world-file" {
-  name       = "files/hello-world.py"
-  source     = "../files/hello-world.py"
-  bucket     = "terraform-test-files"
-  depends_on = [google_storage_bucket.project-files]
-}
+# resource "google_storage_bucket_object" "hello-world-file" {
+#   name       = "files/hello-world.py"
+#   source     = "../files/hello-world.py"
+#   bucket     = "terraform-test-files"
+#   depends_on = [google_storage_bucket.project-files]
+# }
